1320cb597423870e54e80ec1e5fddda04da377dc
Killua <elatarmohamed158@gmail.com>
2024-02-07 02:41:19 +0200

    logindesign

--- app/src/main/java/com/graduation/mawruth/ui/login/LoginActivity.kt
+++ app/src/main/java/com/graduation/mawruth/ui/login/LoginActivity.kt
@@ -2,12 +2,25 @@ package com.graduation.mawruth.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 import com.graduation.mawruth.R
+import com.graduation.mawruth.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var viewBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        //sad
+
+        viewBinding = ActivityLoginBinding.inflate(layoutInflater)
+
+        setContentView(viewBinding.root)
+        initViews()
     }
+    private fun initViews() {
+        WindowCompat.setDecorFitsSystemWindows(window , false)
+    }
+
+
 }
