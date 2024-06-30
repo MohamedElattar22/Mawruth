4ef6bccdca42df538ebff2428c1738dba6a368a4
Killua <elatarmohamed158@gmail.com>
2024-06-30 05:31:03 +0300

    Start of master

--- domain/src/main/java/com/graduation/domain/useCase/sendFavouriteMuseumUseCase.kt
+++ domain/src/main/java/com/graduation/domain/useCase/sendFavouriteMuseumUseCase.kt
@@ -1,14 +1,15 @@
 package com.graduation.domain.useCase
 
-import com.graduation.domain.model.VerificationResponse
+import com.graduation.domain.model.Favourite.FavouriteMuseumResponse
 import com.graduation.domain.repositories.FavouriteMuseumsRepository
 import javax.inject.Inject
 
-class sendFavouriteMuseumUseCase @Inject constructor(
-    private val favouriteMuseumsRepository: FavouriteMuseumsRepository,
+class SendFavouriteMuseumUseCase@Inject constructor(
+    private  val favouriteMuseumsRepository: FavouriteMuseumsRepository,
 
     ) {
-    suspend fun sendFavouriteMuseums(museumId: Int): VerificationResponse? {
-        return favouriteMuseumsRepository.sendFavouriteMuseums(museumId)
+    suspend fun sendFavouriteMuseums(MuseumId : Int): FavouriteMuseumResponse? {
+        return favouriteMuseumsRepository.sendFavouriteMuseums(MuseumId)
     }
+
 }
